7146892871b02568e14c2488eb8e5f418733f57e
sophiem <sophie.mullen@cegeka.com>
2020-09-18 09:01:17 +0100

    add failing test to check multi branch pipeline

--- src/test/java/com/skeleton/SkeletonApplicationTests.java
+++ src/test/java/com/skeleton/SkeletonApplicationTests.java
@@ -3,11 +3,14 @@ package com.skeleton;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SkeletonApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isEqualTo(false);
 	}
 
 }
